085626ac32185f3a05b4fff76cc018633452767a
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-01-15 16:29:24 +0100

    15,01,16,29

---
+++ Ćwiczenia/newjavascript.js
@@ -0,0 +1,25 @@
+
+
+
+       
+
+
+$( ".square"    ).click(function() {
+  $( ".square" ).animate({ "margin-top": "100px", "height": "0px" }, 800  );
+  $( ".square2" ).animate({ "margin-top": "100px", "height": "0px" }, 1200  );
+  $( ".square3" ).animate({ "margin-top": "100px", "height": "0px" }, 1000  );
+  $( ".square4" ).animate({ "margin-top": "100px", "height": "0px" }, 500  );
+ 
+
+});
+ 
+
+  
+
+
+
+
+
+              
+              
+    
---
+++ Ćwiczenia2/newjavascript.js
@@ -0,0 +1,23 @@
+
+
+
+       
+
+
+$( ".square" ).click(function() {
+  $( ".square" ).animate({ "margin-top": "200px", "height": "0px" }, 2000  );
+  $( ".square2" ).animate({ "margin-top": "200px", "height": "0px" }, 1000  );
+  $( ".square3" ).animate({ "margin-top": "200px", "height": "0px" }, 3000  );
+  $( ".square4" ).animate({ "margin-top": "200px", "height": "0px" }, 3500  );
+  $( ".square5" ).animate({ "margin-top": "200px", "height": "0px" }, 500  );
+ 
+
+});
+ 
+
+
+
+
+              
+              
+    
